b67f6983cf15c396facbcd188579cf7a96e74080
stokesbw1 <ikebosedilekae@gmail..com>
2022-11-16 22:29:25 +0200

    implemented clean architecture structure

---
+++ lib/features/popular_articles/domain/reositories/article_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:ny_times_api_test_flutter/core/error/failures.dart';
+import 'package:ny_times_api_test_flutter/features/popular_articles/domain/entities/arcticle.dart';
+
+abstract class ArticleRepository {
+Future<Either<Failure, Article>>  getPopularArticles();
+}
