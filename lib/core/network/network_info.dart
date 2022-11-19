04f099633150bd7415aa45c8b8fc3d77f1cf686d
stokesbw1 <ikebosedilekae@gmail.com>
2022-11-19 22:21:31 +0200

    switched from using provider to bloc - fixed hero image when offline

--- lib/core/network/network_info.dart
+++ lib/core/network/network_info.dart
@@ -1,12 +1,11 @@
-import 'package:simple_connection_checker/simple_connection_checker.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 abstract class NetworkInfo {
   Future<bool> get isConnected;
 }
 
 class NetworkInfoImpl implements NetworkInfo {
-
   @override
-  Future<bool> get isConnected async =>await SimpleConnectionChecker.isConnectedToInternet();
-
+  Future<bool> get isConnected async =>
+      await InternetConnectionCheckerPlus().hasConnection;
 }
---
+++ lib/features/popular_articles/presentation/widgets/article_item.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ny_times_api_test_flutter/features/popular_articles/domain/entities/arcticle.dart';
+
+class ArticleItem extends StatelessWidget {
+  final Article article;
+  final bool isConnected;
+
+  const ArticleItem(
+      {super.key, required this.article, required this.isConnected});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipOval(
+              child: isConnected
+                  ? FadeInImage.assetNetwork(
+                      height: 70,
+                      width: 70,
+                      placeholder: "assets/images/nyt_placeholder.png",
+                      image: article.heroImage)
+                  : Image.asset(
+                      height: 70,
+                      width: 70,
+                      "assets/images/nyt_placeholder.png"),
+            ),
+            Expanded(
+              child: Container(
+                // height: 70,
+                padding: const EdgeInsets.only(left: 10),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      child: Text(
+                        article.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: false,
+                        style: GoogleFonts.poppins(
+                          textStyle: Theme.of(context).textTheme.displayMedium,
+                          height: 1.2,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      article.byline,
+                      style: GoogleFonts.poppins(
+                        textStyle: Theme.of(context).textTheme.displayMedium,
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black54,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.deepOrangeAccent,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.bookmark_outline,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
